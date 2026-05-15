6feb13696c188425c098b6990b818e77ece8971d
TheRedKardinal <claudio_giasi@icloud.com>
2026-05-15 17:50:44 +0200

    Commit js post chiusra esame

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,102 +16,102 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 const prodotti = [
-  {
-    nome: "Smartphone Galaxy S23",
-    categoria: "Elettronica",
-    prezzo: 799.00,
-    rating: 4.8,
-    immagine: "assets/img/smartphone.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Il Signore degli Anelli - Trilogia",
-    categoria: "Blu-ray",
-    prezzo: 25.50,
-    rating: 5.0,
-    immagine: "assets/img/lotr.jpg",
-    disponibile: true
-  },
-  {
-    nome: "T-Shirt Kappa",
-    categoria: "Abbigliamento",
-    prezzo: 15.99,
-    rating: 4.2,
-    immagine: "assets/img/tshirt.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Macchina da Caffè Frog",
-    categoria: "Casa",
-    prezzo: 120.00,
-    rating: 4.5,
-    immagine: "assets/img/caffe.jpg",
-    disponibile: false
-  },
-  {
-    nome: "Cuffie Bluetooth",
-    categoria: "Elettronica",
-    prezzo: 250.00,
-    rating: 4.9,
-    immagine: "assets/img/cuffie.jpg",
-    disponibile: true
-  },
-  {
-    nome: "L'isola del Tesoro",
-    categoria: "Libri",
-    prezzo: 12.00,
-    rating: 4.7,
-    immagine: "assets/img/1984.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Jeans Levi's",
-    categoria: "Abbigliamento",
-    prezzo: 85.00,
-    rating: 4.6,
-    immagine: "assets/img/jeans.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Aspirapolvere Robot",
-    categoria: "Casa",
-    prezzo: 199.90,
-    rating: 4.3,
-    immagine: "assets/img/robot.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Smart TV 55 Pollici 4K",
-    categoria: "Elettronica",
-    prezzo: 450.00,
-    rating: 4.4,
-    immagine: "assets/img/tv.jpg",
-    disponibile: false
-  },
-  {
-    nome: "Guida Definitiva al BodyBuilding",
-    categoria: "Libri",
-    prezzo: 45.00,
-    rating: 4.8,
-    immagine: "assets/img/js-book.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Giacca a Vento Impermeabile",
-    categoria: "Abbigliamento",
-    prezzo: 60.00,
-    rating: 4.1,
-    immagine: "assets/img/giacca.jpg",
-    disponibile: true
-  },
-  {
-    nome: "Set di Coltelli da Cucina",
-    categoria: "Casa",
-    prezzo: 35.50,
-    rating: 4.5,
-    immagine: "assets/img/coltelli.jpg",
-    disponibile: true
-  }
+    {
+        nome: "Smartphone Galaxy S23",
+        categoria: "Elettronica",
+        prezzo: 799.00,
+        rating: 4.8,
+        immagine: "assets/img/smartphone.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Il Signore degli Anelli - Trilogia",
+        categoria: "Blu-ray",
+        prezzo: 25.50,
+        rating: 5.0,
+        immagine: "assets/img/lotr.jpg",
+        disponibile: true
+    },
+    {
+        nome: "T-Shirt Kappa",
+        categoria: "Abbigliamento",
+        prezzo: 15.99,
+        rating: 4.2,
+        immagine: "assets/img/tshirt.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Macchina da Caffè Frog",
+        categoria: "Casa",
+        prezzo: 120.00,
+        rating: 4.5,
+        immagine: "assets/img/caffe.jpg",
+        disponibile: false
+    },
+    {
+        nome: "Cuffie Bluetooth",
+        categoria: "Elettronica",
+        prezzo: 250.00,
+        rating: 4.9,
+        immagine: "assets/img/cuffie.jpg",
+        disponibile: true
+    },
+    {
+        nome: "L'isola del Tesoro",
+        categoria: "Libri",
+        prezzo: 12.00,
+        rating: 4.7,
+        immagine: "assets/img/1984.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Jeans Levi's",
+        categoria: "Abbigliamento",
+        prezzo: 85.00,
+        rating: 4.6,
+        immagine: "assets/img/jeans.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Aspirapolvere Robot",
+        categoria: "Casa",
+        prezzo: 199.90,
+        rating: 4.3,
+        immagine: "assets/img/robot.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Smart TV 55 Pollici 4K",
+        categoria: "Elettronica",
+        prezzo: 450.00,
+        rating: 4.4,
+        immagine: "assets/img/tv.jpg",
+        disponibile: false
+    },
+    {
+        nome: "Guida Definitiva al BodyBuilding",
+        categoria: "Libri",
+        prezzo: 45.00,
+        rating: 4.8,
+        immagine: "assets/img/js-book.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Giacca a Vento Impermeabile",
+        categoria: "Abbigliamento",
+        prezzo: 60.00,
+        rating: 4.1,
+        immagine: "assets/img/giacca.jpg",
+        disponibile: true
+    },
+    {
+        nome: "Set di Coltelli da Cucina",
+        categoria: "Casa",
+        prezzo: 35.50,
+        rating: 4.5,
+        immagine: "assets/img/coltelli.jpg",
+        disponibile: true
+    }
 ];
 
 /* VERSIONE INTERMEDIA — formattaPrezzo(prezzo)
@@ -132,7 +132,17 @@ function formattaPrezzo(prezzo) {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function piazzaStelline(n) {
+    if (n === 0) return '☆☆☆☆☆';
+    if (n === 1) return '★☆☆☆☆';
+    if (n === 2) return '★★☆☆☆';
+    if (n === 3) return '★★★☆☆';
+    if (n === 4) return '★★★★☆';
+    if (n === 5) return '★★★★★';
+    return 'Rating non valido';
+}
 
+console.log(piazzaStelline(6));
 /* VERSIONE INTERMEDIA — Filtro per categoria
    Funzione che riceve il parametro categoria e filtra l'array prodotti mostrando solo i prodotti della categoria ricevuta
 */
